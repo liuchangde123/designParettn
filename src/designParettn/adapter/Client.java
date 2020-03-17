9293d78049cc26c6d91acc6b0872e8cec121319c
liuchangde123 <1564674149@qq.com>
2020-03-17 15:11:28 +0800

    分隔符

--- src/designParettn/adapter/Client.java
+++ src/designParettn/adapter/Client.java
@@ -2,6 +2,7 @@ package designParettn.adapter;
 
 public class Client {
     public static void main(String[] args) {
+        System.out.println("----");
         new Phone(new MyAdapter()).listenMusic();
     }
 }
